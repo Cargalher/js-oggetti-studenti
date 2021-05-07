ad3afd2c79fc90773eef905f7e929c811466ec7b
Cargalher <cargalher@gmail.com>
2021-05-07 15:11:47 +0200

    part one_object created with 3 properties

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 /*Istruzioni:
-Creare un oggetto che descriva uno studente, con le seguenti proprietà:
-nome,
-cognome
-età.
-Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-Creare un array di oggetti di studenti.
-Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.*/
+1. Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+2. Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+3. Creare un array di oggetti di studenti.
+4. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+5.Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.*/
+
+
+
+// 1.
+var student = {
+    'name' : 'Federico',
+    lastName : 'Celia',
+    age:  2021 - 1989,
+} 
